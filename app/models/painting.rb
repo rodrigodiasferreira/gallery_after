330020434f968ac72b2ce9b4f0a967cfce9e3407
Rodrigo Dias Ferreira <rodrigodias@gmx.de>
2013-10-30 22:58:25 -0200

    Working javascript with Rails 4
    Signed-off-by: Rodrigo Dias Ferreira <rodrigodias@gmx.de>

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,5 +1,5 @@
 class Painting < ActiveRecord::Base
-  attr_accessible :image, :name
+  #attr_accessible :image, :name
   mount_uploader :image, ImageUploader
 
   before_create :default_name
@@ -7,4 +7,10 @@ class Painting < ActiveRecord::Base
   def default_name
     self.name ||= File.basename(image.filename, '.*').titleize if image
   end
+  
+  private
+    def painting_params
+      params.require(:painting).permit(:image, :name)
+    end
+  
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,8 +4,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
-  include Sprockets::Helpers::RailsHelper
-  include Sprockets::Helpers::IsolatedHelper
+  #include Sprockets::Helpers::RailsHelper
+  #include Sprockets::Helpers::IsolatedHelper
 
   storage :file
 
